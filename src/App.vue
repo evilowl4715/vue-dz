58c0eeaf3d9da05b2ee92fd422b95c23a2156058
evilowl <evilowl4715@gmail.com>
2025-04-27 17:05:49 +0300

    button

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 let date = new Date();
+import Button from './components/Button.vue';
 </script>
 
 <template>
 	<header></header>
 	<main>{{ date }}</main>
-	<footer>для повторки</footer>
+	<footer>
+		<Button>Button</Button>
+	</footer>
 </template>
 
 <style scoped></style>
---
+++ src/components/Button.vue
@@ -0,0 +1,31 @@
+<script setup></script>
+
+<template>
+	<button class="btn"><slot /></button>
+</template>
+
+<style scoped>
+.btn {
+	background: var(--color-blue);
+	color: var(--color-white);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border-radius: 100px;
+	max-width: 335px;
+	width: 100%;
+	height: 68px;
+	transition: all 0.3s ease;
+	border: none;
+	cursor: pointer;
+	font-family: var(--font);
+	font-weight: 400;
+	font-size: 24px;
+	line-height: 150%;
+	text-align: center;
+}
+
+.btn:hover {
+	background: var(--color-blue-hover);
+}
+</style>
